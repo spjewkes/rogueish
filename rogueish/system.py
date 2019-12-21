78cdb0f92d80827d53f300b2b0cfa33792452a29
Steve Jewkes <steve@mulus.net>
2019-12-21 12:59:26 +0000

    Updated code to use python tcod

--- rogueish/system.py
+++ rogueish/system.py
@@ -4,7 +4,8 @@
 This module defines all system classes.
 """
 
-import tdl
+import tcod
+import tcod.event
 
 from rogueish.component import ComponentPosition, ComponentSprite, ComponentKeyboardControlled
 from rogueish.entity import Entity
@@ -20,22 +21,26 @@ class SystemKeyboard(object):
         """
         Get keyboard input and handle entities that can deal with this.
         """
-        user_input = tdl.event.key_wait()
-        key = user_input.keychar
+        events = tcod.event.get()
 
         for e in entities:
             if e.has(ComponentPosition) and e.has(ComponentKeyboardControlled):
                 x, y = e.get(ComponentPosition).get()
-                if key == "UP":
-                    y -= 1
-                elif key == "DOWN":
-                    y += 1
-                elif key == "LEFT":
-                    x -= 1
-                elif key == "RIGHT":
-                    x += 1
-                elif key == "ESCAPE" or key == 'q':
-                    self.quit = True
+                for event in events:
+                    if event.type == "KEYDOWN":
+                        if event.scancode == tcod.event.SCANCODE_UP:
+                            y -= 1
+                        elif event.scancode == tcod.event.SCANCODE_DOWN:
+                            y += 1
+                        elif event.scancode == tcod.event.SCANCODE_LEFT:
+                            x -= 1
+                        elif event.scancode == tcod.event.SCANCODE_RIGHT:
+                            x += 1
+                        elif event.scancode == tcod.event.SCANCODE_Q or \
+                             event.scancode == tcod.event.SCANCODE_ESCAPE:
+                            self.quit = True
+                    elif event.type == "QUIT":
+                        self.quit = True
                 e.get(ComponentPosition).set(x, y)
 
 class SystemDisplay(object):
@@ -44,7 +49,9 @@ class SystemDisplay(object):
     """
     def __init__(self, width, height):
         self.size = (width, height)
-        self.console = tdl.init(width, height)
+        tcod.console_set_custom_font("prestige12x12_gs_tc.png", tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE)
+        self.console = tcod.console_init_root(width, height, order='F')
+        tcod.console_flush()
 
     def update(self, entities):
         """
@@ -57,6 +64,6 @@ class SystemDisplay(object):
                 position = e.get(ComponentPosition).get()
                 char = e.get(ComponentSprite).get_char()
                 color = e.get(ComponentSprite).get_color()
-                self.console.draw_char(*position, char=char, fg=color)
+                self.console.print(*position, string=char, fg=color)
 
-        tdl.flush()
+        tcod.console_flush()
